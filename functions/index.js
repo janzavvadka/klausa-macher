ed01f8b1829679394ab7d00f45647b70bde69137
MariaLubinska <marysialubinska@gmail.com>
2019-11-30 14:20:14 +0100

    KM-3 data format

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
+let date = new Date();
+let formatted = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 
 app.get('/timestamp', (request, response) => {
     response.set('Cache-Control', '/public, max-age=10, s-mexage=20')
-    response.send(`${Date.now()}`);
+    response.send(`${formatted}`);
 });
 
 exports.app = functions.https.onRequest(app); 
